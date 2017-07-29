c3b35aaa93f21952d3e43daedab1ce91e1adf7ed
farzer <farzer@farzerdeMacBook-Pro.local>
2017-07-29 18:00:45 +0800

    update

---
+++ src/component/index.js
@@ -0,0 +1,18 @@
+import '../scss/main.scss';
+import Button from './Button';
+import Checkbox from './Checkbox';
+import Flex from './Flex';
+import FileUpload from './FileUpload';
+import Radio from './Radio';
+import Sheet from './Sheet';
+import Tip from './Tip';
+
+module.exports = {
+  Button,
+  Checkbox,
+  Flex,
+  FileUpload,
+  Radio,
+  Sheet,
+  Tip
+};
